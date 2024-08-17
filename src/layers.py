1ce4c2026d265ddc9c50175ac4b642a829bc7691
Gilad F <freidkin.gilad@gmail.com>
2024-08-17 23:54:00 +0300

    fixed the PositionalEncoder layer.

--- src/layers.py
+++ src/layers.py
@@ -94,21 +94,29 @@ class LinearInvariant(nn.Module):
         return reduced
 
 
-# TODO: FIX SOME STUFF
 class PositionalEncoding(nn.Module):
 
-    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
+    def __init__(self, d_model: int, max_len: int = 5000):
+        super().__init__()
 
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
         pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer("pe", pe)
+
+        if d_model % 2 != 0:
+            pe[:, 1::2] = torch.cos(position * div_term)[:, 0:-1]
+        else:
+            pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
+
+        # register_buffer => Tensor which is not a parameter, but should be part of the modules state.
+        # Used for tensors that need to be on the same device as the module.
+        # persistent=False tells PyTorch to not add the buffer to the state dict (e.g. when we save the model)
+        self.register_buffer("pe", pe, persistent=False)
 
     def forward(self, x: Tensor) -> Tensor:
-        x = x + self.pe[: x.size(0), :]
-        return self.dropout(x)
+        x = x + self.pe[:, : x.size(1), :]
+        return x
