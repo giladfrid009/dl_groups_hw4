08d53ea7d5f326e5b08bd641a28bb816fa39dcdf
Gilad F <freidkin.gilad@gmail.com>
2024-08-17 21:57:51 +0300

    removed the LinearChanneled layer since the same result can be achieved using regular nn.Linear layer over the flattened data.
    some code cleanup and improvements

--- src/layers.py
+++ src/layers.py
@@ -3,55 +3,6 @@ from torch import Tensor
 from torch import nn
 
 
-class LinearChanneled(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, in_features: int, out_features: int) -> None:
-        """
-        Multi channel linear layer.
-
-        Args:
-            in_channels (int): Number of input channels.
-            out_channels (int): Number of output channels.
-            in_features (int): Number of input features.
-            out_features (int): Number of output features.
-        """
-        super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.in_features = in_features
-        self.out_features = out_features
-        self.bias = torch.nn.Parameter(torch.randn(in_channels, out_channels, out_features))
-        self.weights = torch.nn.Parameter(torch.randn(in_channels, out_channels, out_features, in_features))
-
-    def forward(self, x: Tensor) -> Tensor:
-        """
-        Args:
-            x (Tensor): Input tensor of shape (batch_size, in_features, in_channels).
-
-        Returns:
-            Tensor: Output tensor of shape (batch_size, out_features, out_channels).
-        """
-        assert x.ndim == 3
-        assert x.shape[1] == self.in_features
-        assert x.shape[2] == self.in_channels
-
-        # shape (batch_size, in_channels, in_features)
-        x = x.permute(0, 2, 1)
-
-        # shape (batch_size, in_channels, 1, in_features, 1)
-        x = x.view(-1, self.in_channels, 1, self.in_features, 1)
-
-        # shape (batch_size, in_channels, out_channels, out_features)
-        all = (self.weights @ x).squeeze(-1) + self.bias.unsqueeze(0)
-
-        # shape (batch_size, out_channels, out_features)
-        reduced = torch.mean(all, dim=1, keepdim=False)
-
-        # shape (batch_size, out_features, out_channels)
-        reduced = reduced.permute(0, 2, 1)
-
-        return reduced
-
-
 class LinearEquivariant(nn.Module):
     def __init__(self, in_channels: int, out_channels: int) -> None:
         """
