d9f58753f07c406ad43ea772509866e0bf37e840
Gilad F <freidkin.gilad@gmail.com>
2024-08-28 13:18:08 +0300

    everything is finalized.

--- experiments.py
+++ experiments.py
@@ -1,6 +1,7 @@
 import time
-from tqdm.auto import tqdm
+from tqdm import tqdm
 from typing import Iterable, Any
+import logging
 
 import torch
 from torch import optim
@@ -10,9 +11,13 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 
 import lightning as lit
+from lightning.pytorch import LightningDataModule
+from lightning.pytorch.tuner.tuning import Tuner
 from lightning.pytorch import callbacks, Trainer
 from lightning.pytorch.loggers import TensorBoardLogger
 from lightning.fabric.utilities import throughput
+from lightning.fabric.utilities.seed import seed_everything
+
 
 from src.gaussian_dataset import GaussianDataset
 from src.layers import LinearEquivariant, LinearInvariant, PositionalEncoding
@@ -34,9 +39,10 @@ MODEL_NAMES = [
 
 
 class LitModel(lit.LightningModule):
-    def __init__(self, model: nn.Module):
+    def __init__(self, model: nn.Module, learning_rate: float = 0.001):
         super().__init__()
         self.model = model
+        self.learning_rate = learning_rate
 
     def forward(self, x: Tensor) -> Tensor:
         y_hat: Tensor = self.model(x)
@@ -66,9 +72,81 @@ class LitModel(lit.LightningModule):
         self.log("test_acc", acc, on_epoch=True)
 
     def configure_optimizers(self) -> dict[str, Any]:
-        optimizer = optim.Adam(self.parameters(), lr=0.001)
+        optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
         scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.5, patience=50)
         return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_loss"}
+
+
+class GaussianDataModule(LightningDataModule):
+    def __init__(
+        self,
+        sample_shape: tuple[int, int],
+        train_size: int,
+        test_size: int,
+        batch_size: int = 32,
+        num_workers: int = 0,
+    ):
+        super().__init__()
+        self.sample_shape = sample_shape
+        self.train_size = train_size
+        self.test_size = test_size
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def setup(self, stage: str) -> None:
+
+        if stage == "fit" or stage is None:
+            self.ds_train = GaussianDataset(
+                num_samples=self.train_size,
+                shape=self.sample_shape,
+                var1=1.0,
+                var2=0.8,
+                static=False,
+            )
+
+            self.ds_val = GaussianDataset(
+                num_samples=self.test_size,
+                shape=self.sample_shape,
+                var1=1.0,
+                var2=0.8,
+                static=True,
+            )
+
+        if stage == "test" or stage is None:
+            self.ds_test = GaussianDataset(
+                num_samples=self.test_size,
+                shape=self.sample_shape,
+                var1=1.0,
+                var2=0.8,
+                static=True,
+            )
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.ds_train,
+            batch_size=self.batch_size,
+            shuffle=False,
+            drop_last=False,
+            pin_memory=True,
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.ds_val,
+            batch_size=self.batch_size,
+            shuffle=False,
+            drop_last=False,
+            pin_memory=True,
+        )
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.ds_test,
+            batch_size=self.batch_size,
+            shuffle=False,
+            drop_last=False,
+            pin_memory=True,
+        )
 
 
 def create_mlp_model(n: int, d: int) -> nn.Module:
@@ -109,79 +187,85 @@ def create_invariant_model(n: int, d: int) -> nn.Module:
     )
 
 
-def get_models(n: int, d: int) -> Iterable[tuple[nn.Module, str]]:
+def get_models(n: int, d: int, model_names: list[str] | None = None) -> Iterable[tuple[nn.Module, str]]:
 
     # Canonical models
-
-    model = CanonicalModel(create_mlp_model(n, d))
     model_name = "canonical-mlp"
-    yield model, model_name
+    if model_names is not None and model_name in model_names:
+        model = CanonicalModel(create_mlp_model(n, d))
+        yield model, model_name
 
-    model = CanonicalModel(create_transformer_model(n, d))
     model_name = "canonical-attn"
-    yield model, model_name
+    if model_names is not None and model_name in model_names:
+        model = CanonicalModel(create_transformer_model(n, d))
+        yield model, model_name
 
     # Symmetry models
 
-    # commented out since the model is too slow, so we dont want to run experiments on it
-    # additionally, even creating the permutations is very slow, because there are n! permutations
-
-    """ model = SymmetryModel(
-        model=create_mlp_model(n, d),
-        perms=list(create_all_permutations(n)),
-        chunksize=10,
-    )
     model_name = "symmetry-mlp"
-    yield model, model_name """
+    if model_names is not None and model_name in model_names:
+        model = SymmetryModel(
+            model=create_mlp_model(n, d),
+            perms=list(create_all_permutations(n)),
+            chunksize=10,
+        )
+        yield model, model_name
 
-    """ model = SymmetryModel(
-        model=create_transformer_model(n, d),
-        perms=list(create_all_permutations(n)),
-        chunksize=10,
-    )
     model_name = "symmetry-attn"
-    yield model, model_name """
+    if model_names is not None and model_name in model_names:
+        model = SymmetryModel(
+            model=create_transformer_model(n, d),
+            perms=list(create_all_permutations(n)),
+            chunksize=10,
+        )
+        yield model, model_name
 
     # Sampled symmetry models
 
-    num_perms = 10
-    model = SymmetryModel(
-        model=create_mlp_model(n, d),
-        perms=[Permutation(torch.randperm(n)) for _ in range(num_perms)],
-        chunksize=10,
-    )
     model_name = "symmetry-sampling-mlp"
-    yield model, model_name
+    if model_names is not None and model_name in model_names:
+        num_perms = 10
+        model = SymmetryModel(
+            model=create_mlp_model(n, d),
+            perms=[Permutation(torch.randperm(n)) for _ in range(num_perms)],
+            chunksize=10,
+        )
+        yield model, model_name
 
-    model = SymmetryModel(
-        model=create_transformer_model(n, d),
-        perms=[Permutation(torch.randperm(n)) for _ in range(num_perms)],
-        chunksize=10,
-    )
     model_name = "symmetry-sampling-attn"
-    yield model, model_name
+    if model_names is not None and model_name in model_names:
+        num_perms = 10
+        model = SymmetryModel(
+            model=create_transformer_model(n, d),
+            perms=[Permutation(torch.randperm(n)) for _ in range(num_perms)],
+            chunksize=10,
+        )
+        yield model, model_name
 
     # Intrinsic models
 
-    model = create_invariant_model(n, d)
     model_name = "intrinsic"
-    yield model, model_name
+    if model_names is not None and model_name in model_names:
+        model = create_invariant_model(n, d)
+        yield model, model_name
 
     # Augmented models
 
-    model = nn.Sequential(
-        RandomPermute(),
-        create_mlp_model(n, d),
-    )
     model_name = "augmented-mlp"
-    yield model, model_name
+    if model_names is not None and model_name in model_names:
+        model = nn.Sequential(
+            RandomPermute(),
+            create_mlp_model(n, d),
+        )
+        yield model, model_name
 
-    model = nn.Sequential(
-        RandomPermute(),
-        create_transformer_model(n, d),
-    )
     model_name = "augmented-attn"
-    yield model, model_name
+    if model_names is not None and model_name in model_names:
+        model = nn.Sequential(
+            RandomPermute(),
+            create_transformer_model(n, d),
+        )
+        yield model, model_name
 
 
 def auto_device() -> torch.device:
@@ -263,10 +347,7 @@ def run_time_benchmarks(
 
     input = torch.randn(input_batch, seq_len, feature_dim).to(device)
 
-    for model, model_name in get_models(seq_len, feature_dim):
-
-        if model_names is not None and model_name not in model_names:
-            continue
+    for model, model_name in get_models(seq_len, feature_dim, model_names):
 
         inference_time = measure_inference_time(model=model, input=input, device=device, repeats=repeats)
         training_time = measure_training_time(model=model, input=input, device=device, repeats=repeats)
@@ -287,10 +368,9 @@ def run_flops_benchmarks(
 
     input = torch.randn(input_batch, seq_len, feature_dim).to(device)
 
-    for model, model_name in get_models(seq_len, feature_dim):
+    for model, model_name in get_models(seq_len, feature_dim, model_names):
 
-        if model_names is not None and model_name not in model_names:
-            continue
+        model = model.to(device)
 
         model_fwd = lambda: model(input)
         fwd_flops = throughput.measure_flops(model, model_fwd)
@@ -314,10 +394,7 @@ def run_invariance_tests(
 
     input = torch.randn(5, seq_len, feature_dim).to(device)
 
-    for model, model_name in get_models(seq_len, feature_dim):
-
-        if model_names is not None and model_name not in model_names:
-            continue
+    for model, model_name in get_models(seq_len, feature_dim, model_names):
 
         invariant = test_invariant(
             model=model,
@@ -359,47 +436,32 @@ def run_experiments(
     feature_dim: int,
     train_size: int,
     model_names: list[str] = None,
+    seed: int | None = None,
 ) -> None:
+
+    logging.getLogger("lightning.pytorch").setLevel(logging.ERROR)
+
+    if seed is not None:
+        seed_everything(seed, workers=True)
 
     test_size = 1000
 
-    ds_train = GaussianDataset(num_samples=train_size, shape=(seq_len, feature_dim), var1=1.0, var2=0.8, static=False)
-    ds_val = GaussianDataset(num_samples=test_size, shape=(seq_len, feature_dim), var1=1.0, var2=0.8, static=True)
-    ds_test = GaussianDataset(num_samples=test_size, shape=(seq_len, feature_dim), var1=1.0, var2=0.8, static=True)
-
-    dl_train = DataLoader(
-        dataset=ds_train,
-        batch_size=32,
-        shuffle=True,
-        num_workers=7,
-        persistent_workers=True,
-        drop_last=True,
-        pin_memory=True,
+    data = GaussianDataModule(
+        sample_shape=(seq_len, feature_dim),
+        train_size=train_size,
+        test_size=test_size,
+        num_workers=4,
     )
 
-    dl_val = DataLoader(
-        dataset=ds_val,
-        batch_size=32,
-        shuffle=False,
-        num_workers=0,
-        drop_last=True,
-        pin_memory=True,
-    )
+    for model, model_name in get_models(seq_len, feature_dim, model_names):
 
-    dl_test = DataLoader(
-        dataset=ds_test,
-        batch_size=32,
-        shuffle=False,
-        num_workers=0,
-        drop_last=True,
-        pin_memory=True,
-    )
+        print()
+        print("=" * 100)
+        print(f"Running experiment for model {model_name.upper()}")
+        print(f"Train size: {train_size}, sequence length: {seq_len}")
+        print("=" * 100)
 
-    for model, model_name in get_models(seq_len, feature_dim):
-        if model_names is not None and model_name not in model_names:
-            continue
-
-        lightning_model = LitModel(model)
+        model = LitModel(model)
 
         logger = TensorBoardLogger(save_dir=f"lightning_logs", name=f"train{train_size}_seq{seq_len}/{model_name}")
 
@@ -415,25 +477,28 @@ def run_experiments(
 
         summary_callback = callbacks.RichModelSummary(max_depth=3)
 
-        progress_callback = callbacks.RichProgressBar()
-
         trainer = Trainer(
             accelerator="auto",
             strategy="auto",
             devices="auto",
             precision="32-true",
-            max_epochs=1,
+            max_epochs=-1,
             max_time="00:00:30:00",
-            callbacks=[early_stop_callback, checkpoint_callback, summary_callback, progress_callback],
+            callbacks=[early_stop_callback, checkpoint_callback, summary_callback],
             logger=logger,
             log_every_n_steps=50,
             enable_checkpointing=True,
-            benchmark=True,
-            profiler="simple",
-            deterministic=False,
+            benchmark=False,
+            deterministic=seed is not None,
             fast_dev_run=False,
         )
 
-        trainer.fit(lightning_model, dl_train, dl_val)
+        # tuner = Tuner(trainer)
 
-        trainer.test(lightning_model, dl_test, ckpt_path="best", verbose=True)
+        # tuner.scale_batch_size(lightning_model, datamodule=data)
+
+        # tuner.lr_find(lightning_model, datamodule=data)
+
+        trainer.fit(model, datamodule=data)
+
+        trainer.test(model, datamodule=data, ckpt_path="best", verbose=True)
